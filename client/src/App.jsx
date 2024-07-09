e0e3a85248e956b37165d620617854cc3b2795d1
Protyay140 <protyayray69@gmail.com>
2024-07-09 20:11:40 +0530

    pages and routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
+import Home from './pages/home'  
+import SignIn from './pages/signIn'
+import SignUp from './pages/signUp'
+import Dashboard from './pages/dashboard'
+import Project from './pages/project'
+import About from './pages/about'
+
 
 const App = () => {
   return (
-    <div className='text-green-500'>welcome to my new project </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} /> 
+        <Route path="/dashboard" element={<Dashboard />} /> 
+        <Route path='/project' element={<Project />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
