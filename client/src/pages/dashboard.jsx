23c4f352db60ed133b40de1cc4ca66f5a23fb9c0
Protyay140 <protyayray69@gmail.com>
2024-07-12 12:23:53 +0530

    routes protected through middleware

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+  const {currentUser} = useSelector(state=> state.user);
+  const navigate = useNavigate();
+  useEffect(()=>{
+    if(!currentUser){
+      navigate('/sign-in')
+    }    
+  })
   return (
     <div>Dashboard</div>
   )
