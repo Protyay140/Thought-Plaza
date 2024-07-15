748828ffbd904808da389cbfae3f65c756f7ed76
Protyay140 <protyayray69@gmail.com>
2024-07-15 21:10:43 +0530

    get all users api added

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -10,7 +10,7 @@ router.put('/update/:userId', verifyUser, async (req, res, next) => {
         if (req.user.id != req.params.userId) {
             return res.status(401).json({ success: false, message: "not allowed !!!" });
         } else {
-            if(req.body.password){
+            if (req.body.password) {
                 req.body.password = bcryptjs.hashSync(req.body.password, 10);
             }
             const updateUser = await User.findByIdAndUpdate(req.user.id, {
@@ -32,11 +32,11 @@ router.put('/update/:userId', verifyUser, async (req, res, next) => {
     }
 })
 
-router.delete('/delete/:userId',verifyUser,async(req,res,next)=>{
+router.delete('/delete/:userId', verifyUser, async (req, res, next) => {
     try {
-        if(req.user.id != req.params.userId){
+        if (req.user.id != req.params.userId) {
             return res.status(401).json({ success: false, message: "not allowed to delete !!!" });
-        }else{
+        } else {
             const deletedUser = await User.findByIdAndDelete(req.params.userId);
 
             res.status(200).json({ success: true, message: "user successfully deleted !!!" })
@@ -47,9 +47,42 @@ router.delete('/delete/:userId',verifyUser,async(req,res,next)=>{
 })
 
 
-router.post('/signout',async(req,res,next)=>{
+router.post('/signout', async (req, res, next) => {
     try {
-        res.clearCookie('auth_token').status(200).json({success : true,message : "successfully signout"});
+        res.clearCookie('auth_token').status(200).json({ success: true, message: "successfully signout" });
+    } catch (err) {
+        next(err);
+    }
+})
+
+router.get('/getUsers', async (req, res, next) => {
+    try {
+        const user = await User.findOne({_id : req.query.userId});
+        console.log('user admin : ',user.isAdmin);
+        if (!user.isAdmin) {
+            return res.status(401).json({ success: false, message: "user is not allowed to delete !!!" });
+        }
+
+        const limit = req.query.limit || 9;
+        const startFrom = req.query.startFrom || 0;
+        const order = req.query.order == 'asc' ? 1 : -1;
+
+        let users = await User.find({}).skip(startFrom).limit(limit).sort({ createdAt: order })
+
+        users = users.map((user)=>{
+            const {password:pass,...rest} = user._doc;
+            return rest;
+        });
+
+        const totalUsers = await User.countDocuments();
+
+        res.status(200).json({success : true , message : "get users successfully" , 
+            postInfo : {
+                users,
+                totalUsers
+            }
+        })
+
     } catch (err) {
         next(err);
     }
