bfd4acfa88aeca014dc70292270a7b6a8b898eee
Protyay140 <protyayray69@gmail.com>
2024-07-25 03:57:58 +0530

    cors error fixed

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ dotenv.config();
 const port = 3000;
 
 const corsOptions = {
-    origin: 'http://localhost:5173', // Your frontend origin
+    origin: '*', // Your frontend origin
     credentials: true, // Allow credentials (cookies)
 };
 
