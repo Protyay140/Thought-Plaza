49e09dee34ad711ee2916152728435a58ba844db
Protyay140 <protyayray69@gmail.com>
2024-07-31 14:21:03 +0530

    animation added in home page

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -56,13 +56,11 @@ const Home = () => {
       <div className='main-container min-h-screen flex flex-col  gap-2 p-2'>
         <div className='hero  flex flex-col-reverse md:flex-row gap-2 pl-3'>
           <motion.div
-            variants={{
-              hidden: { opacity: 0, y: 5 },
-              visible: { opacity: 1, y: 0 }
-            }}
-            initial='hidden'
-            animate='visible'
-            transition={{ duration: 0.5}}
+
+            initial={{ opacity: 0, x: -100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+
             className="left  md:w-1/2 flex flex-col gap-2">
             <div className="top text-center mt-3">
               <span className='text-2xl font-bold'>Share Your Thoughts, Engage with the World</span>
@@ -91,14 +89,18 @@ const Home = () => {
             </div>
           </motion.div>
           <motion.div
-            initial={{ y: -100 }}
-            animate={{ y: 0 }}
-            transition={{ duration: 0.5}}
+            initial={{ opacity: 0, y: -100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
             className="right  md:w-1/2">
             <img src="home_pic.png" alt="hero_image" />
           </motion.div>
         </div>
-        <div className="key-point flex flex-col md:flex-row gap-2 mt-6">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+          className="key-point flex flex-col md:flex-row gap-2 mt-6">
           <div className='md:w-1/3  flex flex-col text-center gap-2'>
             <div className='  w-1/2 mx-auto'>
               <img src="create_post.png" alt="" />
@@ -132,12 +134,16 @@ const Home = () => {
                 className='font-bold'>Analyze your dashboard</motion.span>
             </div>
           </div>
-        </div>
+        </motion.div>
         <div className="features flex flex-col  text-center mt-20 md:px-48 ">
           <div className='upper flex flex-col'>
-            <div className=''>
+            <motion.div
+              initial={{ opacity: 0, y: 10 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6 }}
+              className=''>
               <span className='text-3xl'>Features</span>
-            </div>
+            </motion.div>
             <div className=''>
               <span className='text-xs'>Discover What You Can Do</span>
             </div>
@@ -145,39 +151,67 @@ const Home = () => {
           <div className='lower  flex flex-col gap-2'>
             <div className='sign-in flex flex-col md:flex-row '>
               <motion.div
+
+                initial={{ opacity: 0, x: -100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.6 }}
                 className='image md:w-1/2  text-center'>
                 <img src="signin.png" alt="signin" className='md:w-96  mx-auto' />
               </motion.div>
-              <div className='desc md:w-1/2   '>
+              <motion.div
+                initial={{ opacity: 0, x: 100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.6 }}
+                className='desc md:w-1/2   '>
                 <p className='first-letter:text-3xl mt-7 text-sm'>
                   Begin your journey by signing into our platform. With a quick and secure sign-in process, you can access your personalized dashboard, stay updated with the latest posts, and engage with the community. Signing in ensures that all your contributions are saved and easily accessible, making your experience seamless and enjoyable
                 </p>
-              </div>
+              </motion.div>
             </div>
             <div className='post flex flex-col-reverse md:flex-row '>
-              <div className='desc md:w-1/2   '>
+              <motion.div
+                initial={{ opacity: 0, x: -100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.6 }}
+                className='desc md:w-1/2   '>
                 <p className='first-letter:text-3xl mt-7 text-sm'>
                   Unleash your creativity by creating posts on topics that matter to you. Our intuitive editor allows you to craft detailed and engaging posts with ease. Share your thoughts, stories, and insights with a global audience. Whether you’re writing a brief update or a comprehensive article, your voice will find its place in our vibrant community.
                 </p>
-              </div>
-              <div className='image md:w-1/2  text-center'>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, x: 100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.6 }}
+                className='image md:w-1/2  text-center'>
                 <img src="post.png" alt="signin" className='md:w-56  mx-auto' />
-              </div>
+              </motion.div>
             </div>
             <div className='sign-in flex flex-col md:flex-row '>
-              <div className='image md:w-1/2  text-center'>
+              <motion.div
+                initial={{ opacity: 0, x: -100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.6 }}
+                className='image md:w-1/2  text-center'>
                 <img src="profile.png" alt="signin" className='md:w-96  mx-auto' />
-              </div>
-              <div className='desc md:w-1/2  items-center '>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, x: 100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.6 }}
+                className='desc md:w-1/2  items-center '>
                 <p className='first-letter:text-2xl mt-7 text-sm '>
                   Stay on top of your interactions with our powerful dashboard. View all your posts, comments, and engagements in one centralized place. Analyze the performance of your content, track audience feedback, and respond to comments. Our dashboard provides you with the insights you need to grow your influence and connect more deeply with your audience.
                 </p>
-              </div>
+              </motion.div>
             </div>
           </div>
         </div>
         <HR />
-        <div className="post  flex flex-col gap-2 mb-5">
+        <motion.div
+          initial={{ opacity: 0, y: 10 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+          className="post  flex flex-col gap-2 mb-5">
           <div className='text-center font-bold'>
             <span>Recent Posts</span>
           </div>
@@ -206,7 +240,7 @@ const Home = () => {
               </div>
             </>
           }
-        </div>
+        </motion.div>
       </div>
     </>
   )
