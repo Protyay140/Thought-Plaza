9924945aecd12f8eb6cf82d91ecebebf11eb85fa
Protyay140 <protyayray69@gmail.com>
2024-09-06 11:53:13 +0530

    bug fixed

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3000',
     'https://thought-plaza.onrender.com',
-    'https://thought-plaza.vercel.app/'
+    'https://thought-plaza.vercel.app'
 ];
 
 // CORS options with a function to check allowed origins
